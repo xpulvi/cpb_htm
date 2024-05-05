78306501b4ae0d8450326b7f0547ab30a7ad312c
xpulvi <pulvirenti.n.a@gmail.com>
2024-05-06 00:15:49 +0200

    fix fetc post logic_ad_pepol
    ho Davor mi a messo gli id delle persone su logica_vista_grafo.
    
    mi a fixato la form fec in javascript

--- add_it/logic_ad_pepol.js
+++ add_it/logic_ad_pepol.js
@@ -1,22 +1,45 @@
 function submitForm() {
+    /*
+    const form = document.getElementById('json_ad_pepol');
+    const formData = new FormData(form);
+    
+
+    */
+    let requestBody = {
+        "name": "",
+        "descriptib_name": "",
+        "surname": "",
+        "nikename": "",
+        "who_get_nickname": "",
+        "work": "",
+        "and_wealthy": "",
+        "age_at_death": "",
+        "first_appears": "",
+        "img": ""
+    };
+
+
     const form = document.getElementById('json_ad_pepol');
     const formData = new FormData(form);
 
-    console.log(formData); 
+    //formData.append("name", "Pomegranate");
+
+    
+    for (const pair of formData.entries()) {
+        console.log(pair[0], pair[1]);
+        if(requestBody.hasOwnProperty(pair[0]) && pair[0] != "img"){
+            requestBody[pair[0]] = pair[1];
+        } //else if trasforma il file img in base 64. stringa.
+        
+    }
+    
+
+      console.log(JSON.stringify(requestBody));
     fetch('http://localhost:8080/person/insert', {
         method: 'POST',
-        body: JSON.stringify({
-            "name":name,
-            "descriptib_name":descriptib_name,
-            "surname":surname,
-            "nikename":nikename,
-            "who_get_nickname":who_get_nickname,
-            "work":work,
-            "and_wealthy":and_wealthy,
-            "age_at_death":age_at_death,
-            "first_appears":first_appears,
-            "img":img
-        }),
+        body: JSON.stringify(requestBody),
+        //body: formData,
+
         headers: {
             'Content-Type': 'application/json'
         }
@@ -35,3 +58,11 @@ function submitForm() {
         alert('Si è verificato un errore durante l\'invio dei dati.');
     });
 }
+
+const form = document.getElementById('json_ad_pepol');
+
+form.addEventListener('submit', function(event) {
+    event.preventDefault(); // Evita il comportamento predefinito di invio del form
+    console.log('Form inviato correttamente!');
+    submitForm(); // Chiama la funzione per inviare il form
+});
--- grafo/logica_vista_grafo.js
+++ grafo/logica_vista_grafo.js
@@ -50,44 +50,63 @@ async function getElencoPersone() {
 
 // fine codice ripetuto.  ----------------------------------------------
 
+async function getElencoPersone() {
+  try {   
+      const response = await fetch('http://localhost:8080/person/list');
+      console.log(response);
+      if (!response.ok) {
+        throw new Error('Errore nella risposta dell\'API');
+      }
+      console.log(response);
+      const data = await response.json();
+      console.log(data); 
+      return data; 
 
-function main () {
+  } catch (error) {
+    console.log(response);
+      console.error('Errore durante la chiamata API:', error.message);
+      return null; 
+  }
+}
+
+async function main () {
+
+
+
+  const myPersons = await getElencoPersone();
+  console.log(myPersons)
             
     var graph = Viva.Graph.graph();
 
     // Construct the graph
-    graph.addNode('anvaka', {url : 'https://secure.gravatar.com/avatar/91bad8ceeec43ae303790f8fe238164b'});
-    graph.addNode('manunt', {url : 'https://secure.gravatar.com/avatar/c81bfc2cf23958504617dd4fada3afa8'});
-    graph.addNode('thlorenz', {url : 'https://secure.gravatar.com/avatar/1c9054d6242bffd5fd25ec652a2b79cc'});
-    graph.addNode('bling', {url : 'https://secure.gravatar.com/avatar/24a5b6e62e9a486743a71e0a0a4f71af'});
-    graph.addNode('diyan', {url : 'https://secure.gravatar.com/avatar/01bce7702975191fdc402565bd1045a8?'});
-    graph.addNode('pocheptsov', {url : 'https://secure.gravatar.com/avatar/13da974fc9716b42f5d62e3c8056c718'});
-    graph.addNode('dimapasko', {url : 'https://secure.gravatar.com/avatar/8e587a4232502a9f1ca14e2810e3c3dd'});
-    graph.addNode('eva', {url : 'D:\Immagini\icone\edit.svg'});
-    graph.addNode('john', {url : ''});
-
-    graph.addLink('anvaka', 'manunt');
-    graph.addLink('anvaka', 'thlorenz');
-    graph.addLink('anvaka', 'bling');
-    graph.addLink('anvaka', 'diyan');
-    graph.addLink('anvaka', 'pocheptsov');
-    graph.addLink('anvaka', 'dimapasko');
-    graph.addLink('anvaka', 'john');
-
-    graph.addLink('john', 'eva');
+    //graph.addNode(getElencoPersone())
+    myPersons.forEach(element => {
+      graph.addNode(element.idPerson, {label: element.idPerson, url : 'https://secure.gravatar.com/avatar/91bad8ceeec43ae303790f8fe238164b'});
+    });
     
     // Set custom nodes appearance
     var graphics = Viva.Graph.View.svgGraphics();
+    
     graphics.node(function(node) {
         // The function is called every time renderer needs a ui to display node
-        return Viva.Graph.svg('image')
+        var ui = Viva.Graph.svg('g');
+        let imgG = Viva.Graph.svg('image')
                 .attr('width', 24)
                 .attr('height', 24)
                 .link(node.data.url); // node.data holds custom object passed to graph.addNode();
-        })
+        let textG = Viva.Graph.svg('text').attr('y', '-4').text(node.data.label)
+
+        ui.append(imgG)
+                ui.append(textG)
+                return ui
+              })
         .placeNode(function(nodeUI, pos){
             // Shift image to let links go to the center:
-            nodeUI.attr('x', pos.x - 12).attr('y', pos.y - 12);
+            //nodeUI.attr('x', pos.x - 12).attr('y', pos.y - 12);
+            nodeUI.attr('transform',
+            'translate(' +
+                  (pos.x - 12) + ',' + (pos.y - 12) +
+            ')');
         });
 
     var renderer = Viva.Graph.View.renderer(graph, {
